b6c4722726d17da4fe57bb6320418856e0eee16e
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-07-27 10:49:44 -0400

    Added Main Form

---
+++ COMP123-M2020-Lesson10/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_Lesson10
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
